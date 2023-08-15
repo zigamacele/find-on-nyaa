94ef598af48e4eeb8182889daaecb1dd2a77f123
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-08-15 18:53:36 +0200

    feat: update website icon href

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Find on Nyaa
-// @homepageURL  https://github.com/
-// @supportURL   https://github.com/
-// @namespace    https://greasyfork.org/en/scripts/
+// @homepageURL  https://github.com/zigamacele/find-on-nyaa
+// @supportURL   https://github.com/zigamacele/find-on-nyaa
 // @version      0.1
-// @description  ""
+// @description  Changes the website icon link on Livechart to redirect to Nyaa and sorts by seeders in descending order.
 // @author       Ziga Macele
 // @license      MIT
 // @match        https://www.livechart.me/**
@@ -15,5 +14,29 @@
 (function () {
   'use strict';
 
-  // Your code here...
+  const nyaaSearch = 'https://nyaa.si/?f=0&c=0_0&q=';
+  const nyaaSorting = '&s=seeders&o=desc';
+
+  function updateIconLink(iconSelector, title) {
+    iconSelector.href = `${nyaaSearch}${title}${nyaaSorting}`;
+  }
+
+  function processAnimeCard(animeCard) {
+    const titleSelector = animeCard.querySelector(
+      '[data-anime-card-target="mainTitle"]'
+    );
+    const iconSelector = animeCard.querySelector('.website-icon');
+
+    if (iconSelector && titleSelector) {
+      const title = titleSelector.textContent.split(' ').slice(0, 2).join(' ');
+      updateIconLink(iconSelector, title);
+    }
+  }
+
+  function run() {
+    const animeCards = document.querySelectorAll('.anime-card');
+    animeCards.forEach(processAnimeCard);
+  }
+
+  run();
 })();
